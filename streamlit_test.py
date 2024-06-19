3823b71a8e26b6a66eebba3ea11ff959e5129435
carrotfarm357 <itzmede@gmail.com>
2024-06-19 22:48:24 +0900

    Update streamlit_test.py

--- streamlit_test.py
+++ streamlit_test.py
@@ -2,7 +2,7 @@ import streamlit as st
 from PIL import Image
 
 # 사이드바 만들기
-st.sidebar.header("로그인")
+st.sidebar.header("Login")
 user_id = st.sidebar.text_input("ID", value='', max_chars=20)
 user_pw = st.sidebar.text_input("PW", value="", type='password')
 
@@ -13,7 +13,7 @@ if user_id == 'abcd' and user_pw == '1234':
     st.sidebar.write("select>> ", user_opt)
 
     # 메인 화면
-    st.subheader("main")
+    st.subheader("Bao Family")
     image_files=["welcome.jpg","le.jpg","ai.jpg","fu.jpg","rui.jpg","hui.jpg"]
     sel_index = sel_options.index(user_opt)
     img_file = image_files[sel_index]
