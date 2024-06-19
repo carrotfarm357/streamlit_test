8cbc95b2cc603e42701c5e8774a10624c81a311e
carrotfarm357 <itzmede@gmail.com>
2024-06-19 22:41:39 +0900

    Update streamlit_test.py

--- streamlit_test.py
+++ streamlit_test.py
@@ -9,12 +9,12 @@ user_pw = st.sidebar.text_input("PW", value="", type='password')
 if user_id == 'abcd' and user_pw == '1234':
     st.sidebar.header("image list")
     sel_options = ["", 'le', 'ai', 'fu', 'rui', 'hui']
-    user_opt = st.sidebar.selectbox("like", sel_options, index = 0)
+    user_opt = st.sidebar.selectbox("like", sel_options, index=0)
     st.sidebar.write("select>> ", user_opt)
 
     # 메인 화면
     st.subheader("main")
-    image_files=["welcome.jpg","le.jpg","ai.jpg","fu.jpg","rui.jpg","hui.jpg"]
+    image_files=["welcome.jpeg","le.jpg","ai.jpg","fu.jpg","rui.jpg","hui.jpg"]
     sel_index = sel_options.index(user_opt)
     img_file = image_files[sel_index]
     img_local = Image.open(img_file)
